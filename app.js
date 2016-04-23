1baf45cce1749ad91e459d724ace4fe228c3ab14
Vinicius Domingues <vinicius.ld@outlook.com>
2016-04-23 17:43:42 -0300

    Mundanças no app.js: - Criacação de uma factory para os posts para que eles possa ser exportado para outros controllers (Procurar mais detalhes sobre factory) - Como o two-way data-binding so ocorre na variavel "$scope" foi necessario igualar ela ao factory posts e chamar a função construtora(?)
    Mudanças no index.html:
    - O curso não se aprofunda muito no bootstrap, mas basicamente ele adiciona o bootstrap de um CDN (content delivery network)
    - Adiciona a classe row a div principal e adiciona os tamanhos de acordo com o tamanho de cada layout
    - Utiliza mais algumas coisas basicas de bootstrap como os glyphicons e a classe page-header

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var app = angular.module('flapperNews', []);
 
+
+app.factory('posts',[function(){
+    var o = {
+        posts : []
+    }
+    
+    return o;
+}])
+
 app.controller('MainCtrl', [
     '$scope',
-    function ($scope) {
+    'posts',
+    function ($scope, posts) {
         $scope.test = 'Hello world!';
-        $scope.posts = [
-            { title: 'post 1', upvotes: 5 },
-            { title: 'post 2', upvotes: 2 },
-            { title: 'post 3', upvotes: 15 },
-            { title: 'post 4', upvotes: 9 },
-            { title: 'post 5', upvotes: 4 }
-        ];
+        $scope.posts = posts.posts;
 
         $scope.incrementUpvotes = function (post) {
             post.upvotes += 1;
@@ -28,3 +32,4 @@ app.controller('MainCtrl', [
         };
 
     }]);
+
