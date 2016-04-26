9c9e9efa9313d39223ceb0f0d530ccbd3f599c20
Vinicius Domingues <vinicius.ld@outlook.com>
2016-04-25 22:55:37 -0300

    Mudanças no index.html - Começando a utilizar o ui-router do angular. - Foi retirado o ng-controller da tag body pois o próprio ui-router fica responsavel por essa configuracao - Foi adicionada a tag ui-view para que o router saiba onde renderizar as views - Foi criado um template dentro da tag script. Nessa tag script foi especificado um id para que o router consiga apontar para ela ao inves de um arquivo externo e tb foi especificado como ng-template (pesquisar mais sobre)
    Mudanças no app.js:
    - Importação do ui.router
    - Definição do nome pro state('home'), da url ('/home'), do template ('/home.html' referenciado na tag script) e do controller responsavel pelo template.
    - Configurado que caso recebe alguma requisicao atraves de outra url, ela sera redirecionada para o state home.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+           
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            });
+        $urlRouterProvider.otherwise('home');
+    }]);
 
-
-app.factory('posts',[function(){
+app.factory('posts', [function () {
     var o = {
-        posts : []
+        posts: []
     }
-    
+
     return o;
 }])
 
